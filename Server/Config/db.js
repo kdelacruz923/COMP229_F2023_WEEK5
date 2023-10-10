abc8eb76e8712e9040ec9b2f465efb20033eb1c3
karendelacruz <kdelac14@my.centennialcollege.ca>
2023-10-10 17:14:35 -0400

    Initial commit

---
+++ Server/Config/db.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hostname = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://localhost:2707/clothing_store";
+exports.Hostname = "localhost";
+//# sourceMappingURL=db.js.map
